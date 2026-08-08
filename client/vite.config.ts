fa7f48d431d679579380584d74cc75bb43edcaad
Balykin.Nikolay <nikola10wwww@mail.ru>
2026-08-08 12:45:37 +0300

    commit changes

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,13 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+
 export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      '/api': 'http://localhost:5167',
-      '/health': 'http://localhost:5000',
-    },
+      '/api': {
+        target: 'http://localhost:5167',
+        changeOrigin: true,
+      },
+      '/health': {
+        target: 'http://localhost:5167',
+        changeOrigin: true,
+      },
+    }
   },
 })
